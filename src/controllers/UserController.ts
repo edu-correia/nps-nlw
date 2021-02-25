5ef4cb8701adb909020dd55eaa85279948a2b60d
Eduardo Correia <eduardo.correia.ehsc@gmail.com>
2021-02-25 20:45:07 -0300

    Criação do controller de surveys

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,14 +1,14 @@
 import { Request, Response } from 'express';
-import { getRepository } from 'typeorm';
-import { User } from '../models/User';
+import { getCustomRepository } from 'typeorm';
+import { UsersRepository } from '../repositories/UsersRepository';
 
 class UserController {
     async create(req: Request, res: Response){
         const {name, email} = req.body;
 
-        const usersRepository = getRepository(User);
+        const usersRepository = getCustomRepository(UsersRepository);
         
-        const userAlreadyExists = usersRepository.findOne({email});
+        const userAlreadyExists = await usersRepository.findOne({email});
 
         if(userAlreadyExists) return res.status(400).json({error: "User already exists"})
 
@@ -22,4 +22,5 @@ class UserController {
     }
 }
 
-export {UserController};
+export { UserController };
+
--- src/routes.ts
+++ src/routes.ts
@@ -1,11 +1,15 @@
 import { Router } from 'express';
 
 import {UserController} from './controllers/UserController';
+import {SurveysController} from './controllers/SurveysController';
 
 const routes = Router();
 
 const userController = new UserController();
+const surveysController = new SurveysController();
 
 routes.post("/users", userController.create);
+routes.post("/surveys", surveysController.create);
+routes.get("/surveys", surveysController.show);
 
 export {routes};
