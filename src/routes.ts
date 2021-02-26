484382b247a9dfefea284ae4247bd9cbdb0d03bf
Eduardo Correia <eduardo.correia.ehsc@gmail.com>
2021-02-26 16:40:14 -0300

    Envio básico de emails

--- src/routes.ts
+++ src/routes.ts
@@ -2,14 +2,19 @@ import { Router } from 'express';
 
 import {UserController} from './controllers/UserController';
 import {SurveysController} from './controllers/SurveysController';
+import {SendMailController} from './controllers/SendMailController';
 
 const routes = Router();
 
 const userController = new UserController();
 const surveysController = new SurveysController();
+const sendMailController = new SendMailController();
 
 routes.post("/users", userController.create);
-routes.post("/surveys", surveysController.create);
+
 routes.get("/surveys", surveysController.show);
+routes.post("/surveys", surveysController.create);
+
+routes.post("/sendMail", sendMailController.execute);
 
 export {routes};
