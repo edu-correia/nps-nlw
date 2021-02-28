e31a51182e7f6d1325767883083c35b86e591bf7
Eduardo Correia <eduardo.correia.ehsc@gmail.com>
2021-02-27 23:39:11 -0300

    Tratamento de erros

--- src/controllers/AnswerController.ts
+++ src/controllers/AnswerController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import { getCustomRepository } from "typeorm";
+import { AppError } from "../errors/AppError";
 import { SurveysUsersRepository } from "../repositories/SurveysUsersRepository";
 
 class AnswerController {
@@ -11,7 +12,9 @@ class AnswerController {
 
         const surveyUser = await surveysUsersRepository.findOne({id: String(u)});
 
-        if(!surveyUser) return res.status(400).json({error: "Survey User does not exists"});
+        if(!surveyUser) {
+            throw new AppError("Survey User does not exists")
+        }
 
         surveyUser.value = Number(id);
 
--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -2,6 +2,7 @@ import { Request, Response } from 'express';
 import { getCustomRepository } from 'typeorm';
 import { UsersRepository } from '../repositories/UsersRepository';
 import * as yup from 'yup';
+import { AppError } from '../errors/AppError';
 
 class UserController {
     async create(req: Request, res: Response){
@@ -15,14 +16,14 @@ class UserController {
         try {
             await schema.validate(req.body, {abortEarly: false});
         } catch (error) {
-            return res.status(400).json({error});
+            throw new AppError(error);
         }
 
         const usersRepository = getCustomRepository(UsersRepository);
         
         const userAlreadyExists = await usersRepository.findOne({email});
 
-        if(userAlreadyExists) return res.status(400).json({error: "User already exists"})
+        if(userAlreadyExists) throw new AppError("User already exists");
 
         const user = usersRepository.create({
             name, email
